0153bda72d6cfec1f9bd636bf13276bf59bb3ee0
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-05 23:55:46 +0200

    ++

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
-function addNum(num1 = 1, num2 = 1) {
-    return (num1 + num2);
-};
+// function addNum(num1 = 1, num2 = 1) {
+//     return (num1 + num2);
+// };
 
-console.log(addNum());
+// console.log(addNum());
+
